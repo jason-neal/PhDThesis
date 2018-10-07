82e2fe496c0f80ae6341af934b144c2be24651b1
Jason Neal <jason.neal@astro.up.pt>
2018-10-07 13:23:52 +0100

    Precison tables, spirou, nirps

--- data/precision_results/spirou_latex_table.py
+++ data/precision_results/spirou_latex_table.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import pandas as pd
 
-nirps = pd.read_csv("nirps_etc_logg_4.5_ref_J_snr_100_oct2018.csv", header=0)
+spirou = pd.read_csv("new_spirou_etc_logg_4.5_ref_self_snr_100_oct2018.csv", header=0)
 
-selected = nirps[["temp", "band", "vsini", "resolution",
+
+selected = spirou[["temp", "band", "vsini", "resolution",
                       "cond1", "cond2", "cond3"]]
 
 # Sorting
-rorder = ["75k","100k"]
+rorder = ["60k","80k","100k"]
 rorderdict = dict(zip(rorder,range(len(rorder))))
 border = ["Z","Y","J","H","K"]
 borderdict = dict(zip(border,range(len(border))))
